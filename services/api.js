eede2ec1cdcd7e1cd196d260a9b151f3cedff843
carlos_casanova <carlosctu94@gmail.com>
2022-08-27 19:23:05 -0300

    [CHANGE] Refactoring and fixing the query string on /tweets endpoint

--- services/api.js
+++ services/api.js
@@ -1,11 +1,14 @@
 import cors from "cors";
 import express from "express";
 
+//#region 
+// Incializando o express
 const app = express();
 // Permitindo que o Front se comunique com o back
 app.use(cors());
 // Dizendo pro express que os dados virão em json e ele precisará fazer o parse
 app.use(express.json());
+//#endregion
 
 const users = [];
 const tweets = [];
@@ -19,80 +22,60 @@ app.post("/sign-up", (req, res) => {
   }
   // add username: username e avatar: avatar
   users.push({ username, avatar });
-  res.status(201).send({ message: "Ok", user: { username, avatar } });
+  res.status(201).send({ message: "OK", user: { username, avatar } });
 });
 
 app.post("/tweets", (req, res) => {
   const { tweet } = req.body;
   const username = req.get("User");
+  // Fast fail
   if (!tweet || !username) {
     return res.status(400).send("Todos os campos são obrigatórios!");
   }
+  // #Refatorando
+  // Adicionando o avatar no post do tweet para retirar o find dos anteriores commits
+  // nos endpoints (get) abaixo
+  const avatar = users.find((user) => user.username === username);
 
-  tweets.push({
-    // Adicionando um id que vai aumentando conforme a qtd dos tweets
+  // Adicionando o último tweet no começo para facilitar o tratamento dos dados
+  tweets.unshift({
     id: tweets.length + 1,
     username,
+    avatar: avatar.avatar,
     tweet,
   });
 
-  res.status(201).send({ message: "Ok", tweet: { username, tweet } });
+  res.status(201).send({ message: "OK", tweet: { username, tweet } });
 });
 
 app.get("/tweets", (req, res) => {
   const { page } = req.query;
-  // Fast fail: caso a página comece em zero
+  // Como o query string é opcional, fazemos um ternario
+  // para atribuir um pageNumb = 1 caso page não vier
+  const pageNumb = page ? parseInt(page, 10) : 1;
+  // Fast fail
   if (!pageNumb > 0) {
     return res.status(400).send("Informe uma página válida!");
   }
-  
-  const pageNumb = parseInt(page, 10);
+
   let lastTweets = [];
 
-  console.log(page);
-  // Caso o query string (página) seja > 1
-  if (pageNumb > 1 && tweets.length >= pageNumb * 10 - 10) {
-    for (let i = 0; i < tweets.length - 10; i++) {
-      const userAvatar = users.find(
-        (user) =>
-          tweets[tweets.length - 10 - (i + 1)].username === user.username
-      );
-      lastTweets.push({
-        ...tweets[tweets.length - 10 - (i + 1)],
-        avatar: userAvatar.avatar,
-      });
-    }
-    return res.status(200).send(lastTweets);
+  if (pageNumb > 1) {
+    const end = pageNumb * 10;
+    const start = pageNumb * 10 - 10;
+    return res.status(200).send(tweets.slice(start, end));
   }
 
   for (let i = 0; i < tweets.length; i++) {
     if (lastTweets.length === 10) break;
-    let lastTweet = tweets.length - (i + 1);
-
-    const userAvatar = users.find(
-      (user) => tweets[lastTweet].username === user.username
-    );
-
-    lastTweets.push({
-      ...tweets[lastTweet],
-      avatar: userAvatar.avatar,
-    });
+    lastTweets.push(tweets[i]);
   }
   res.status(200).send(lastTweets);
 });
 
 app.get("/tweets/:username", (req, res) => {
-  const username = req.params.username;
-  const avatar = users.find((user) => user.username === username);
-
-  let userTweets = [];
-
-  tweets.map((tweet) => {
-    if (tweet.username === username) {
-      userTweets.push({ ...tweet, avatar: avatar.avatar });
-    }
-  });
-
+  const { username } = req.params;
+  const userTweets = tweets.filter((tweet) => tweet.username === username);
   res.status(200).send(userTweets);
 });
 
