3bce7cf22621bf92d4b7a76ee9e9534d6dd638a4
carlos_casanova <carlosctu94@gmail.com>
2022-08-27 12:19:34 -0300

    [CHANGE] query string tweets get endpoint

--- services/api.js
+++ services/api.js
@@ -1,58 +1,74 @@
 import cors from "cors";
 import express from "express";
 
-// Inicializando o express
 const app = express();
-// subir o servidor local na web e utilizar ele
+// Permitindo que o Front se comunique com o back
 app.use(cors());
-// Dizendo pro express que iremos ter dados em JSON
-// e que vai precisa fazer um parse para que o js possa ler eles
+// Dizendo pro express que os dados virão em json e ele precisará fazer o parse
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
 const users = [];
 const tweets = [];
 
 app.post("/sign-up", (req, res) => {
-  // Object.keys pega os ids do objeto
-  // Object.values pega os valores dos ids
-  const keys = Object.keys(req.body);
-  const values = Object.values(req.body);
-  const validateKeys = keys[0] === "username" && keys[1] === "avatar";
-  const validateValues = values[0] && values[1];
-
-  if (validateKeys && validateValues) {
-    users.push(req.body);
-    res.status(201).send("Ok");
-  } else {
-    res.status(400).send("Todos os campos são obrigatórios!");
+  // Desestruturação
+  const { username, avatar } = req.body;
+  // Fast fail
+  if (!username || !avatar) {
+    return res.status(400).send({ error: "Todos os campos são obrigatórios!" });
   }
+  // add username: username e avatar: avatar
+  users.push({ username, avatar });
+  res.status(201).send({ message: "Ok", user: { username, avatar } });
 });
 
 app.post("/tweets", (req, res) => {
-  const user = req.get("User");
-  const keys = Object.keys(req.body);
-  const values = Object.values(req.body);
-  // const validateKeys = keys[0] === "username" && keys[1] === "tweet";
-  // const validateValues = values[0] && values[1];
-  const validateKeys = user && keys[0] === "tweet";
-  const validateValues = values[0];
-
-  if (validateKeys && validateValues) {
-    tweets.push({ id: tweets.length + 1, ...req.body });
-    res.status(201).send("Ok");
-  } else {
-    res.status(400).send("Todos os campos são obrigatórios!");
+  const { tweet } = req.body;
+  const username = req.get("User");
+  if (!tweet || !username) {
+    return res.status(400).send("Todos os campos são obrigatórios!");
   }
+
+  tweets.push({
+    // Adicionando um id que vai aumentando conforme a qtd dos tweets
+    id: tweets.length + 1,
+    username,
+    tweet,
+  });
+
+  res.status(201).send({ message: "Ok", tweet: { username, tweet } });
 });
 
 app.get("/tweets", (req, res) => {
+  const { page } = req.query;
+  // Fast fail: caso a página comece em zero
+  if (!pageNumb > 0) {
+    return res.status(400).send("Informe uma página válida!");
+  }
+  
+  const pageNumb = parseInt(page, 10);
   let lastTweets = [];
+
+  console.log(page);
+  // Caso o query string (página) seja > 1
+  if (pageNumb > 1 && tweets.length >= pageNumb * 10 - 10) {
+    for (let i = 0; i < tweets.length - 10; i++) {
+      const userAvatar = users.find(
+        (user) =>
+          tweets[tweets.length - 10 - (i + 1)].username === user.username
+      );
+      lastTweets.push({
+        ...tweets[tweets.length - 10 - (i + 1)],
+        avatar: userAvatar.avatar,
+      });
+    }
+    return res.status(200).send(lastTweets);
+  }
+
   for (let i = 0; i < tweets.length; i++) {
     if (lastTweets.length === 10) break;
-
     let lastTweet = tweets.length - (i + 1);
-    
+
     const userAvatar = users.find(
       (user) => tweets[lastTweet].username === user.username
     );
@@ -62,7 +78,6 @@ app.get("/tweets", (req, res) => {
       avatar: userAvatar.avatar,
     });
   }
-
   res.status(200).send(lastTweets);
 });
 
